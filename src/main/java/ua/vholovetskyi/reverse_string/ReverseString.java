10549ddb97658eea4a8a3fe3b44830981eb29c90
Volodymyr Holovetskyi <vholovetskyi@gmail.com>
2024-03-15 15:31:43 +0100

    Reverse string use Stack

--- src/main/java/ua/vholovetskyi/reverse_string/ReverseString.java
+++ src/main/java/ua/vholovetskyi/reverse_string/ReverseString.java
@@ -1,11 +1,16 @@
 package ua.vholovetskyi.reverse_string;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class ReverseString {
 
     public static void main(String[] args) {
         var str = "STRINGS";
         System.out.println(reverseStringUseStringBuilder(str));
         System.out.println(reverseStringUseCharArray(str));
+        System.out.println(reverseStringUseStringBuilder2(str));
+        System.out.println(reverseStringUseStack(str));
     }
 
     /**
@@ -18,10 +23,10 @@ public class ReverseString {
     }
 
     /**
-     * The second way to use char
+     * The second way to use char[]
      */
     public static String reverseStringUseCharArray(String str) {
-        char[]chars = str.toCharArray();
+        char[] chars = str.toCharArray();
 
         for (int i = 0; i < chars.length / 2; i++) {
             char temp = chars[i];
@@ -31,4 +36,31 @@ public class ReverseString {
         return String.valueOf(chars);
     }
 
+    /**
+     * The third way to use StringBuilder 2
+     */
+
+    public static String reverseStringUseStringBuilder2(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            sb.append(str.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    /**
+     * The fourth way to use Stack
+     */
+    public static String reverseStringUseStack(String str) {
+        Deque<Character> stack = new ArrayDeque<>() {
+        };
+        for (int i = 0; i < str.length(); i++) {
+            stack.push(str.charAt(i));
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+        return sb.toString();
+    }
 }
